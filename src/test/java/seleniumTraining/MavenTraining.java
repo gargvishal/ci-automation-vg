c4b106020b2520060ab71b1e1218e0eac01dd4eb
Vishal Garg <vishal.garg@office365.3pillarglobal.com>
2023-04-09 20:07:56 +0530

    Jenkins work 2 - changed to include web automation and API automation

--- src/test/java/seleniumTraining/MavenTraining.java
+++ src/test/java/seleniumTraining/MavenTraining.java
@@ -3,53 +3,55 @@ package seleniumTraining;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MavenTraining {
-
-	@SuppressWarnings("deprecation")
-	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-
 	
-				//System.setProperty("webdriver.chrome.driver", "D:\\Trainings\\Selenium webdriver\\browser\\chromedriver_win32\\chromedriver.exe");
-			
-			WebDriverManager.chromedriver().setup();
-			ChromeDriver driver = new ChromeDriver();
-				//driver.get("https://www.sugarcrm.com/au/request-demo/");
-			    driver.get("https://demo.mobiscroll.com/javascript/select/multiple-select");
-				driver.manage().window().maximize();
-				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-				
-				//String PageSource = driver.getPageSource();
-				//System.out.println("PageSource will be " + PageSource);
-				//String Title = driver.getTitle();
-				//System.out.println("Title is"+ Title);
-				//WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
-				//driver.findElement(By.id("twotabsearchtextbox")).sendKeys("trimmer");
-				//By searchLocator = RelativeLocator.with(By.tagName("input")).toRightOf(By.id("twotabsearchtextbox"));
-				//WebElement searchLocator = driver.findElement(RelativeLocator.with(By.tagName("input")).toRightOf(By.id("twotabsearchtextbox")));
-				//searchLocator.click();
-				//driver.findElement(By.id("nav-search-submit-button")).click();
-//				driver.findElement(By.id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")).click();
-//				driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-//				WebElement ddown = driver.findElement(By.id("how-select"));
-//				Select select = new Select(ddown);
-//				select.selectByValue("Internet");
-//				Thread.sleep(4000);
-//				select.selectByVisibleText("Word of Mouth/Referral");
-//				Thread.sleep(2000);
-//				select.selectByIndex(1);
-				Thread.sleep(3000);
-				WebElement ddown = driver.findElement(By.id("multiple-select-input"));
-				Select select = new Select (ddown);
-				select.selectByValue("2");
-				select.selectByVisibleText("Electronics & Computers");
-			}
+	public static WebDriver driver;
+	
+	    @BeforeMethod
+		public void launchDriver() {
+		//WebDriverManager.chromedriver().setup();
+		System.setProperty("webdriver.chrome.driver", "D:\\Trainings\\Selenium webdriver\\browser\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+			//driver.get("https://www.sugarcrm.com/au/request-demo/");
+		   // driver.get("https://demo.mobiscroll.com/javascript/select/multiple-select");
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	//@SuppressWarnings("deprecation")
+	
+	@Test
+	public void Test1() {
+			driver.navigate().to("https://www.youtube.com");
+			System.out.println("Page Title 1 : "+ driver.getTitle() );
+	}
+	
+	@Test
+	public void Test2() {
+		driver.navigate().to("https://www.youtube.com");
+		System.out.println("Page Title 2 : "+ driver.getTitle() );
+	}
+	
+	@Test
+	public void Test3() {
+		driver.navigate().to("https://www.youtube.com");
+		System.out.println("Page Title 3 : "+ driver.getTitle() );
+	}
+	
+	@AfterMethod
+	public void quitborwser() {
+		driver.quit();
+	}
 
 	}
